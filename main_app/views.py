f38d74ce41fbc69e1459552cb692001ad52449e6
dani-diaz <daniela.diaz0808@gmail.com>
2022-05-06 16:16:01 -0700

    first commit

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,36 @@
+import numbers
+from tokenize import Number
+from turtle import numinput
 from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+# Add the Minion class & list and view function below the imports
+class Minion:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, hair, description, eyes):
+    self.name = name
+    self.hair = hair
+    self.description = description
+    self.eyes = eyes
+
+minions = [
+  Minion('kevin the Minion', 'black', 'usually golf gear', 2),
+  Minion('Bob the Minion', 'tortoise shell', 'diluted tortoise shell', 0),
+  Minion('Dave the Minion', 'black tripod', '3 legged cat', 4)
+]
+
+
+def home(request):
+  return HttpResponse('<h1>My Minions Page</h1>')
+
+
+def about(request):
+  return render(request, 'about.html')
+
+def minions_index(request):
+  return render(request, 'minions/index.html', {
+    'minions': minions,
+  })
+
 
 # Create your views here.
